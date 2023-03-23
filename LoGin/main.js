1605e1f83cefbc73e4dc80a3877db6da15b207a4
huyle1212 <zxckuhuyzxc@gmail.com>
2023-03-24 04:18:49 +0700

    3/24/2023

--- LoGin/main.js
+++ LoGin/main.js
@@ -3,34 +3,44 @@ function dangki(e){
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById ("password").value;
+    var password2 =document.getElementById("password2").value;
     const user = {
         username: username,
         email:email,
         password: password,
+        password2:password2,
 
     };
-    var json = JSON.stringify(user);
-    localStorage.setItem(username,json);
-    alert(
-        "dang ki thanh cong"
-    );
+    if(password!=password2){
+        alert("Mật khẩu không trùng khớp vui lòng nhập lại!")
+    }else{
+        var json = JSON.stringify(user);
+        localStorage.setItem(username,json);
+        window.location.href="/LoGin/dangnhap.html"
+    }
+  
 }
 function dangnhap(e){
     event.preventDefault();
     var email = document.getElementById("email").value;
     var password = document.getElementById ("password").value;
+
     const user = localStorage.getItem(email);
-    var data = JSON.parse(user);
+    let data = JSON.parse(user);
+    
     if(user == null){
         alert("vui lòng nhập lại tài khoản")
     }
     else if(  email == data.email && password == data.password ){
         alert("dang nhap thanh cong")
-        window.location.href="https://www.facebook.com/"
+
+        let currentUser = JSON.stringify(data);
+        localStorage.setItem('currentUser', currentUser);
+        localStorage.getItem('currentUser')
+
+        window.location.href="/index.html"
     }
     else{
         alert("dang nhap that bại")
     }
-
-
 }
--- main.js
+++ main.js
@@ -20,50 +20,81 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
-
-// add cart
-const btn = document.querySelectorAll('.btn')
-var listProduct = [] //list chứa sản phẩm
-
-btn.forEach(function(button, index) {
-  button.addEventListener("click", function(event) {
-    const product = event.target.parentElement
-    const spbanchay = product.querySelector('.spbanchay').innerText
-    const anh = product.querySelector('img').src
-    const price = product.querySelector('.price').innerText
-    const newProduct = {
-      id: Math.random(),
-      spbanchay: spbanchay,
-      anh: anh,
-      price: price
-    }
-    addCart(newProduct)
-
-  });
-})
-//them san pham
+//Hien thi dan sach  san pham vao gio hang
 function showListCart() {
   const item = document.querySelector('.content-cart-item')
   item.innerHTML = ``
   for (let i = 0; i < listProduct.length; i++) {
     item.innerHTML += `
-    <div class="cart-box-mini">
-      <div> <img class="cart-img" style=" pointer-events: none;" src="${listProduct[i].anh} " alt=""></div>
-      <h4 class="cart-sanpham">"${listProduct[i].spbanchay}</h4>
-      <h4 class="cart-price"  >${listProduct[i].price}</h4>
-      <button style="font-size: 18px; color: brown; margin-left: 10px;" onClick = handleDelete(${listProduct[i].id})>Xoá</button>         
+      <div class="cart-box-mini">
+        <div> <img class="cart-img" style=" pointer-events: none;" src="${listProduct[i].img}" alt=""></div>
+        <h4 class="cart-sanpham">"${listProduct[i].title}</h4>
+        <h4 class="cart-price"  >${listProduct[i].price}</h4>
+        <button style="font-size: 18px; color: brown; margin-left: 10px;" onClick = handleDelete(${listProduct[i].id})>Xoá</button>         
       </div>
 
   `
   }
 }
+// add cart
+let listProduct = [] //list chứa sản phẩm trong gio hang
 
 //Thêm sản phẩm vào giỏ hàng
-function addCart(newCart) {
-    listProduct.push(newCart) //đẩy sản phẩm vào mảng
-    showListCart() //gọi hàm để hiển thị sản phẩm
+function addProduct(id) {
+    const newProduct = {
+      id: Math.random(),
+      title: getAllProducts[id].title,
+      img: getAllProducts[id].img,
+      price: getAllProducts[id].price
+    }
+
+  listProduct.push(newProduct) //đẩy sản phẩm vào mảng
+  showListCart() //gọi hàm để hiển thị sản phẩm
 }
 
+
+//danh sach tat ca san pham
+let getAllProducts = [
+  {
+      id: 0,
+      newProduct: true,
+      img: 'img/upload_4229fd8ac4874935850a75888e847287_grande.jpg',
+      title: 'Pin dự phòng Anker PowerCore+ 10050 – A1310',
+      price: '1,320,000 đ'
+  },
+  {
+      id: 1,
+      newProduct: true,
+      img: 'img/upload_4d8a63f0e0794dec84f9432a473deb4f_grande.jpg',
+      title: 'Bao Da Pin Dự Phòng Anker 10050 – A7096081',
+      price: '390,000 đ'
+  },
+  {
+      id: 2,
+      newProduct: false,
+      img: 'img/a1310012_td01_bao_da_grande.png',
+      title: 'Pin dự phòng Anker PowerCore 20100 – A1271',
+      price: '400,000 đ'
+  },
+  {
+    id: 3,
+    img: 'img/a1310012_td01_bao_da_grande.png',
+    title: 'Pin dự phòng Anker PowerCore 20100 – A1271',
+    price: '400,000 đ'
+  }
+]
+
+
+
+
+//logic tim kiem
+console.log(
+  getAllProducts.map(product => {
+    return product.title.toLocaleLowerCase().includes('A1271'.trim().toLocaleLowerCase())
+  })
+);
+
+//Xoa san pham
 function handleDelete(id) {
   const newList = listProduct.filter(item => { //lọc tất cả sản phẩm có id khác id click xóa vào mảng mới
       return item.id != id
@@ -71,23 +102,65 @@ function handleDelete(id) {
   listProduct = newList //gán mảng mới vào danh sách sản phẩm
   showListCart()
 }
-// const list = document.querySelectorAll('.btn')
-// var listProduct = []
+//Đăng xuất
+function logOut() {
+  localStorage.setItem('currentUser', null)
+  window.location.reload()
+}
 
-// btn.forEach(function(button, index) {
-//   button.addEventListener("click", function (event) {
-//     const product = event.target.parentElement
-//     const spbanchay = product.querySelector('.spbanchay').innerText
-//     const anh = product.querySelector('img').src
-//     const price = product.querySelector('.price').innerText
-//     console.log(anh);
-//     const newProduct = {
-//       id: Math.random(),
-//       spbanchay: spbanchay,
-//       anh: anh,
-//       price: price
-//     }
- 
-//   })
-// })
+// them san pham moi vao gio hang
+function showNewProduct () {
+  const products = document.querySelector('.new-product')
+  for (let i = 0; i < getAllProducts.length; i++) {
+    if(getAllProducts[i].newProduct == true) {
+      products.innerHTML += `
+        <div class="sp1" ${getAllProducts[i].id}>
+          <img src="${getAllProducts[i].img}" alt="">
+          <h4 class="spbanchay">${getAllProducts[i].title}</h4>
+          <h4 class="price">${getAllProducts[i].price}</h4>
+          <button class="btn" onclick="addProduct(${getAllProducts[i].id})"> Thêm Vào Giỏ Hàng </button>
+        </div>
+      `    
+    }
+  }
+}
+//hien thi danh sach san pham
+function showProduct () {
+  const products = document.querySelector('.all-product')
+  for (let i = 0; i < getAllProducts.length; i++) {
+    products.innerHTML += `
+      <div class="sp1" ${getAllProducts[i].id}>
+        <img src="${getAllProducts[i].img}" alt="">
+        <h4 class="spbanchay">${getAllProducts[i].title}</h4>
+        <h4 class="price">${getAllProducts[i].price}</h4>
+        <button class="btn" onclick="addProduct(${getAllProducts[i].id})"> Thêm Vào Giỏ Hàng </button>
+    </div>
+    `    
+  }
+}
+
+//khi vao trang web hoac tai lai trang thi chay ham main() dau
+function main() {
+  showProduct()
+  showNewProduct()
+  const login = document.querySelector('.login')
+  
+  let data = localStorage.getItem('currentUser')
+  let currentUser = JSON.parse(data)
+
+  currentUser 
+    ? login.innerHTML = `
+    
+      <h3 class"usernamelogin"> <i class="fa-solid fa-user"></i> ${currentUser?.username}</h3>
+      <h3 class="logOut" onclick="logOut()" style="font-size: small;
+      cursor: pointer; padding: 2px 2px 2px 0px; ">Đăng xuất</h3>
+    `
+    : login.innerHTML = `
+      <a href="LoGin/dangnhap.html" class="login_a">ĐĂNG NHẬP/</a>
+      <a href="LoGin/dangki.html" class="login_a">ĐĂNG KÍ</a>
+    `
+}
+main()
+
+
 
